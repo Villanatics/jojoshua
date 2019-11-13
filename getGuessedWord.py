53bf5913603244a67472b8fe912c110f748f6bb4
Joshie Villa <57549673+Villanatics@users.noreply.github.com>
2019-11-13 22:39:44 +0800

    Delete getGuessedWord.py

--- getGuessedWord.py
+++
@@ -1,6 +0,0 @@
-def getGuessedWord(secretWord, lettersGuessed):
-    for elements in secretWord:
-        if elements not in str(lettersGuessed):
-            secretWord=secretWord.replace(elements,'_')
-            return secretWord
-        
